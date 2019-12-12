e54bb856d5e2bc6687ed3a071bc6a43feccac888
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-12 00:08:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,6 @@
 # Write a speak_to_grandma method.
 
-if speak_to_grandma (I love you grandma!)
- puts"I LOVE YOU TOO PUMPKIN!"
-else 
-  puts "Huh?!" SPEAK UP, SONNY!"
-elsif "No, Not Since 1938!
-elsif
+
    
  
 # Whatever you say to grandma, she should respond with
